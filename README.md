8908807b6935dea07d61a7b5ee8c046e2ee20898
Nicky Khorasani <nicholas.khorasani@gmail.com>
2024-12-18 02:56:00 -0500

    Setting up project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # BareDB
+
+
+
+# Cloning 
+
+git clone <Repo name>
+git submodule update --init --recursive
+
+
+
+### Update Submodules
+git submodule update --remote --merge
---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Clean the previous build
+rm -rf ./build/
+mkdir ./build/
+
+cd ./build/
+
+# Run cmake and make
+cmake ..
+make
+
+# Copy the built executable to the bin/ directory
+mkdir -p ../bin/
+cp ./BareDB/BareDB ../bin/
+
+echo "Build successful. Executable is in the 'bin' directory."
